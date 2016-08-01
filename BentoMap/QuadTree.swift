1459898d500988d3a08fd0078a437aa290d5885a
Rob Visentin <jvisenti@gmail.com>
2016-08-01 13:56:18 -0400

    renamed boundingBox -> bucketRegion

--- BentoMap/QuadTree.swift
+++ BentoMap/QuadTree.swift
@@ -14,13 +14,13 @@ public struct QuadTree<NodeData> {
 
     var ordinalNodes: OrdinalNodes<NodeData>?
 
-    public let boundingBox: BoundingBox
+    public let bucketRegion: BoundingBox
     public let bucketCapacity: Int
     public var points = [QuadTreeNode<NodeData>]()
 
-    public init(boundingBox: BoundingBox = .world, bucketCapacity: Int) {
+    public init(bucketRegion: BoundingBox = .world, bucketCapacity: Int) {
         precondition(bucketCapacity > 0, "Bucket capacity must be greater than 0")
-        self.boundingBox = boundingBox
+        self.bucketRegion = bucketRegion
         self.bucketCapacity = bucketCapacity
     }
 
@@ -72,7 +72,7 @@ public extension QuadTree {
 
 
     public mutating func insertNode(node: QuadTreeNode<NodeData>) -> Bool {
-        guard boundingBox.containsMapPoint(node.mapPoint) else {
+        guard bucketRegion.containsMapPoint(node.mapPoint) else {
             return false
         }
 
@@ -96,8 +96,8 @@ public extension QuadTree {
 private extension QuadTree {
 
     mutating func subdivide() {
-        let trees = boundingBox.quadrants.map { quadrant in
-            QuadTree(boundingBox: quadrant, bucketCapacity: self.bucketCapacity)
+        let trees = bucketRegion.quadrants.map { quadrant in
+            QuadTree(bucketRegion: quadrant, bucketCapacity: self.bucketCapacity)
         }
 
         ordinalNodes = OrdinalNodes(northWest: trees.northWest,
@@ -109,7 +109,7 @@ private extension QuadTree {
     func nodesInRange(range: BoundingBox) -> [QuadTreeNode<NodeData>] {
         var nodes = [QuadTreeNode<NodeData>]()
 
-        guard boundingBox.intersectsBoundingBox(range) else {
+        guard bucketRegion.intersectsBoundingBox(range) else {
             return nodes
         }
 
